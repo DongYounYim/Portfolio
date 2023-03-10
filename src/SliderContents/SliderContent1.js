e632f7de64e872e73e66908bd620c503a52f276d
Im_younique <48540492+DongYounYim@users.noreply.github.com>
2023-03-10 18:13:20 +0900

    [feat] 이미지 파일 제거 후 SliderContent1 생성

---
+++ src/SliderContents/SliderContent1.js
@@ -0,0 +1,65 @@
+// image
+import profileImg from "../assets/images/profile.jpg";
+
+const SliderContent1 = () => {
+  return (
+    <div style={{ display: "flex" }}>
+      <div style={{ flex: 1, textAlign: "center", objectFit: "fill" }}>
+        <img src={profileImg} placeholder="프로필 이미지" />
+      </div>
+      <div style={{ flex: 1, margin: "0 16px" }}>
+        <h2>안녕하세요</h2>
+        <h2>프론트엔드 개발자 임동윤입니다.</h2>
+        <span>
+          현재 충남대학교 컴퓨터공학과 학부 4학년으로 공부 중이며, 프론트,
+          백엔드뿐만 아니라 CS 전반적인 지식을 기반으로 좋은 프로그램을 개발하려
+          노력 중입니다.
+        </span>
+        <div className="history">
+          <div>
+            <h4>2018</h4>
+            <ol>
+              <li>천안 중앙고등학교 48회 졸업</li>
+              <li>충남대학교 컴퓨터공학과 18학번 입학</li>
+            </ol>
+          </div>
+          <div>
+            <h4>2019</h4>
+            <ol>
+              <li>군 휴학</li>
+            </ol>
+          </div>
+          <div>
+            <h4>2021</h4>
+            <ol>
+              <li>육군 예비역 하사 제대 / 군 복학</li>
+              <li>CN Music Festa 홍보 홈페이지 제작</li>
+            </ol>
+          </div>
+          <div>
+            <h4>2022</h4>
+            <ol>
+              <li>Design Sprint 교육 수료</li>
+              <li>2022 컴퓨터 융합학부 학생회 MEMORY 기획부장</li>
+              <li>충남대학교 보안 정보 동아리 ARGOS 기획부장</li>
+              <li>2022 Sillicon Valley Internship 수료</li>
+              <li>천안 월봉고등학교 교육 봉사 (Flutter교육)</li>
+              <li>(주)나라셀라(WINEONE) 디지털 사업부 팀 인턴</li>
+              <li>대흥동 문화의 집 웹 페이지 제작 봉사활동 참여</li>
+            </ol>
+          </div>
+          <div>
+            <h4>2023</h4>
+            <ol>
+              <li>보안 정보 동아리 ARGOS 부회장</li>
+              <li>네이버 커넥트 재단 1784 스쿨혁명 멘토 참여</li>
+              <li>CNU SWAcademy 프론트엔드 과정 수료</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SliderContent1;
--- src/page/Main.js
+++ src/page/Main.js
@@ -1,7 +1,6 @@
-import { Header, Navigation, Banner, Slider } from "../components";
+import { Header, Banner, Slider } from "../components";
 
-// 임시이미지
-import introImage from "../assets/images/intro.png";
+import SliderContent1 from "../SliderContents/SliderContent1";
 
 export default function Main() {
   return (
@@ -12,20 +11,11 @@ export default function Main() {
         <Banner />
         <Slider id="intro">
           <div className="CardContent" id="content1">
-            <img className="CardImage" src={introImage} />
+            <SliderContent1 />
           </div>
-          <div className="CardContent">
-            <div>다양한 프로젝트 경험을 통한 경험치 축적</div>
-          </div>
-          <div className="CardContent">
-            <div>지속적인 공부를 통한 성장 욕심</div>
-          </div>
-          <div className="CardContent">
-            <div>
-              프론트엔드 뿐만아니라 백엔드 Cloud 보안에 모든 관심을 두고
-              있습니다.
-            </div>
-          </div>
+          <div className="CardContent">웹 개발 관련</div>
+          <div className="CardContent">모바일 관련</div>
+          <div className="CardContent">기타</div>
         </Slider>
         <section style={{ height: "100vh" }}>
           <div style={{ display: "flex" }}>
