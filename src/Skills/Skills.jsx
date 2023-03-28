7befb700250c44a31abe440d3d35b596c4c0a33b
DongyounYim <dyyim4725@gmail.com>
2023-03-28 23:26:50 +0900

    [feat] Skills Component 완성

---
+++ src/Skills/Skills.jsx
@@ -0,0 +1,59 @@
+import { Icon } from "@iconify/react";
+
+const Skills = () => {
+  return (
+    <section style={{ backgroundColor: "#FDD36A" }}>
+      <h3 className="sectionTitle">Skills</h3>
+      <div className="gridContainer" style={{}}>
+        <div className="skillSetContainer">
+          <h5>Front-end</h5>
+          <div className="skillIconContainer">
+            <Icon icon="logos:html-5" style={{ fontSize: "6rem" }} />
+            <Icon icon="logos:css-3" style={{ fontSize: "6rem" }} />
+            <Icon icon="logos:javascript" style={{ fontSize: "6rem" }} />
+            <Icon icon="skill-icons:jquery" style={{ fontSize: "6rem" }} />
+            <Icon icon="logos:typescript-icon" style={{ fontSize: "6rem" }} />
+            <Icon icon="logos:react" style={{ fontSize: "6rem" }} />
+            <Icon icon="skill-icons:sass" style={{ fontSize: "6rem" }} />
+            <Icon
+              icon="skill-icons:nextjs-light"
+              style={{ fontSize: "6rem" }}
+            />
+          </div>
+        </div>
+        <div className="skillSetContainer">
+          <h5>Back-end</h5>
+          <div className="skillIconContainer">
+            <Icon icon="logos:python" style={{ fontSize: "6rem" }} />
+            <Icon icon="logos:django-icon" style={{ fontSize: "6rem" }} />
+            <Icon icon="skill-icons:flask-dark" style={{ fontSize: "6rem" }} />
+            <Icon icon="logos:firebase" style={{ fontSize: "6rem" }} />
+            <Icon icon="logos:mysql" style={{ fontSize: "6rem" }} />
+            <Icon icon="simple-icons:oracle" style={{ fontSize: "6rem" }} />
+          </div>
+        </div>
+        <div className="skillSetContainer">
+          <h5>Deploy</h5>
+          <div className="skillIconContainer">
+            <Icon icon="mdi:aws" style={{ fontSize: "6rem" }} />
+            <Icon icon="logos:firebase" style={{ fontSize: "6rem" }} />
+            <Icon icon="logos:vercel" style={{ fontSize: "4rem" }} />
+          </div>
+        </div>
+        <div className="skillSetContainer">
+          <h5>Mobile-App</h5>
+          <div className="skillIconContainer">
+            <Icon icon="skill-icons:react-dark" style={{ fontSize: "6rem" }} />
+            <Icon
+              icon="skill-icons:flutter-light"
+              style={{ fontSize: "6rem" }}
+            />
+            <Icon icon="logos:dart" style={{ fontSize: "6rem" }} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
--- src/page/Main.js
+++ src/page/Main.js
@@ -6,6 +6,7 @@ import {
   SliderContent3,
 } from "../SliderContents";
 import { Aboutme } from "../Aboutme";
+import { Skills } from "../Skills";
 
 export default function Main() {
   return (
@@ -22,19 +23,7 @@ export default function Main() {
           <SliderContent3 />
         </Slider>
       </section>
-      <section style={{ backgroundColor: "#FDD36A" }}>
-        <h3 className="sectionTitle">Skills</h3>
-        <div>
-          <h5>Front-end</h5>
-          <p>HTML5, CSS, JavaScript, jQuery, TypeScript, React, Sass, NextJS</p>
-          <h5>Mobile-App</h5>
-          <p>React Native, Flutter, Dart</p>
-          <h5>Back-end</h5>
-          <p>Python, Django, Flask, Firebase, MySQL, Oracle</p>
-          <h5>Deploy</h5>
-          <span>AWS, Naver Cloud, Firebase, Vercel</span>
-        </div>
-      </section>
+      <Skills />
       <Aboutme />
     </div>
   );
