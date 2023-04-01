999f9e3603e1f9c9919e50ece33fab3cd51337da
Im_younique <48540492+DongYounYim@users.noreply.github.com>
2023-04-01 13:13:24 +0900

    [chore] ImageSlider 네이밍 변경

--- src/components/ImageSlider/ImageSlider.js
+++
@@ -1,5 +0,0 @@
-const ImageSlider = () => {
-  return <div></div>;
-};
-
-export default ImageSlider;
---
+++ src/components/ImageSlider/SliderContainer.js
@@ -0,0 +1,5 @@
+const SliderContainer = () => {
+  return <div></div>;
+};
+
+export default SliderContainer;
