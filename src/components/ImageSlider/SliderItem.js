ce67f35503c3499a15c162b35f5c4027a8ec5b88
DongyounYim <dyyim4725@gmail.com>
2023-04-07 11:08:46 +0900

    [feat] Project 목록 구현 완료

--- src/components/ImageSlider/SliderItem.js
+++ src/components/ImageSlider/SliderItem.js
@@ -4,6 +4,11 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 // imageSet
+import Port1 from "../../assets/images/Port1.png";
+import Todo1 from "../../assets/images/Todo1.png";
+import Todo2 from "../../assets/images/Todo2.png";
+import Todo3 from "../../assets/images/Todo3.png";
+import Todo4 from "../../assets/images/Todo4.png";
 import Wine1 from "../../assets/images/1kmwine1.jpeg";
 import Wine2 from "../../assets/images/1kmwine2.jpeg";
 import Wine3 from "../../assets/images/1kmwine3.jpeg";
@@ -12,6 +17,11 @@ import Booth1 from "../../assets/images/Booth1.jpeg";
 import Booth2 from "../../assets/images/Booth2.jpeg";
 import Booth3 from "../../assets/images/Booth3.jpeg";
 import Booth4 from "../../assets/images/Booth4.jpeg";
+import Face1 from "../../assets/images/Face2.png";
+import Face2 from "../../assets/images/Face1.png";
+import Dae1 from "../../assets/images/Dae1.png";
+import Dae2 from "../../assets/images/Dae2.png";
+import Dae3 from "../../assets/images/Dae3.png";
 import Moon1 from "../../assets/images/Moon1.jpeg";
 import Moon2 from "../../assets/images/Moon2.jpeg";
 import Moon3 from "../../assets/images/Moon3.png";
@@ -20,17 +30,22 @@ import Culture1 from "../../assets/images/Culture1.jpeg";
 import Culture2 from "../../assets/images/Culture2.jpeg";
 import Culture3 from "../../assets/images/Culture3.jpeg";
 import Culture4 from "../../assets/images/Culture4.jpeg";
+import Music1 from "../../assets/images/Music1.png";
+import Music2 from "../../assets/images/Music2.png";
+import Music3 from "../../assets/images/Music3.png";
+import Music4 from "../../assets/images/Music4.png";
+import Music5 from "../../assets/images/Music5.png";
 
 const ImageGroup = {
-  Todo: [],
+  Todo: [Todo1, Todo2, Todo3, Todo4],
   Wine: [Wine1, Wine2, Wine3, Wine4],
   Moon: [Moon1, Moon2, Moon3, Moon4],
   Booth: [Booth1, Booth2, Booth3, Booth4],
   Culture: [Culture1, Culture2, Culture3, Culture4],
-  Port: [],
-  Face: [],
-  Dae: [],
-  Music: [],
+  Port: [Port1],
+  Face: [Face1, Face2],
+  Dae: [Dae1, Dae2, Dae3],
+  Music: [Music1, Music2, Music3, Music4, Music5],
 };
 
 export default function SliderItem({ groupName }) {
